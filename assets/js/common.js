0d58efd867cf2091f01d07b6716433f161e60993
marimonon <mappi-@hotmail.co.jp>
2024-12-23 22:24:10 +0900

    fadeinとcursor

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,4 +7,17 @@
   document.getElementById('drawer_btn').addEventListener('click', toggleDrawer);
   document.getElementById('drawer_shade').addEventListener('click', toggleDrawer);
 
+  var observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-active");
+      }
+    });
+  }, {
+    rootMargin: "0px 0px -30% 0px"
+  });
+  document.querySelectorAll(".fadein").forEach(function (element) {
+    observer.observe(element);
+  });
+
 })();
